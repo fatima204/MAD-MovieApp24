2e15973f4d821a21ab79dcd135c8fddc68a99e2c
fatima204 <fatima.amrin.hossain@gmail.com>
2024-03-03 21:11:53 +0100

    BottomAppBar implemented

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.movieappmad24
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,55 +16,132 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
+data class BottomItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppMAD24Theme {
                 // A surface container using the 'background' color from the theme
+                val items = listOf(
+                    BottomItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home
+                    ),
+                    BottomItem(
+                        title = "WatchList",
+                        selectedIcon = Icons.Filled.Star,
+                        unselectedIcon = Icons.Outlined.Star
+                    )
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MovieList(movies = getMovies())
+                    var selectedItemIndex by rememberSaveable {
+                        mutableStateOf(0)
+                    }
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed{ index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            // navController.navigate(item.title)
+                                        },
+                                        label = {
+                                          Text(text = item.title)  
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if(index == selectedItemIndex){
+                                                    item.selectedIcon
+                                                    }else {
+                                                       item.unselectedIcon
+                                                    },
+                                                contentDescription = item.title
+                                            )
+                                        })
+                                }
+                            }
+                        },
+                        content = {
+                            MovieList(movies = getMovies())
+                        }
+                    )
                 }
             }
         }
     }
-}
 
 @Composable
-fun MovieList(movies: List<Movie> = getMovies()){
+fun MovieList(movies: List<Movie> = getMovies()) {
     LazyColumn {
         items(movies) { movie ->
             MovieRow(movie)
@@ -72,14 +150,15 @@ fun MovieList(movies: List<Movie> = getMovies()){
 }
 
 @Composable
-fun MovieRow(movie: Movie){
+fun MovieRow(movie: Movie) {
     var showDetails by remember {
         mutableStateOf(false)
     }
 
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(5.dp),
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
         shape = ShapeDefaults.Large,
         elevation = CardDefaults.cardElevation(10.dp)
     ) {
@@ -93,17 +172,19 @@ fun MovieRow(movie: Movie){
                 Image(
                     painter = painterResource(id = R.drawable.movie_image),
                     contentScale = ContentScale.Crop,
-                    contentDescription = "placeholder image")
+                    contentDescription = "placeholder image"
+                )
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(10.dp),
                     contentAlignment = Alignment.TopEnd
-                ){
+                ) {
                     Icon(
                         tint = MaterialTheme.colorScheme.secondary,
                         imageVector = Icons.Default.FavoriteBorder,
-                        contentDescription = "Add to favorites")
+                        contentDescription = "Add to favorites"
+                    )
                 }
             }
 
@@ -115,22 +196,27 @@ fun MovieRow(movie: Movie){
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(text = movie.title)
-                Icon(modifier = Modifier
-                    .clickable {
-                        showDetails = !showDetails
-                    },
+                Icon(
+                    modifier = Modifier
+                        .clickable {
+                            showDetails = !showDetails
+                        },
                     imageVector =
                     if (showDetails) Icons.Filled.KeyboardArrowDown
-                    else Icons.Default.KeyboardArrowUp, contentDescription = "show more")
+                    else Icons.Default.KeyboardArrowUp, contentDescription = "show more"
+                )
             }
         }
     }
 }
 
+
 @Preview
 @Composable
-fun DefaultPreview(){
+fun DefaultPreview() {
     MovieAppMAD24Theme {
         MovieList(movies = getMovies())
     }
+}
+
 }
