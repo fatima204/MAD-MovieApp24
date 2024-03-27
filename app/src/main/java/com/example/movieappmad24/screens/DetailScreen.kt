8009d6fa479e7b7686e5400daa3eafec5269b8ca
fatima204 <fatima.amrin.hossain@gmail.com>
2024-03-27 01:50:36 +0100

    Task: DetailScreen c) (show all movie images in a LazyRow) implemented

--- app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
@@ -1,22 +1,42 @@
 package com.example.movieappmad24.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,16 +65,31 @@ fun DetailScreen(movieId: String?, navController: NavController) {
             )
         },
     ) { innerPadding ->
-        Box (
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-        ){
-            movie?.let {MovieRow(movie = it)}
+        ) {
+            movie?.let { MovieRow(movie = it) }
+            movie?.images?.let { MovieDetailsImages(images = it) }
         }
 
-
     }
-
 }
+
+@Composable
+fun MovieDetailsImages(images: List<String>) {
+    LazyRow {
+        items(images) { imageUrl ->
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(200.dp)
+            ) {
+                MovieImage(imageUrl = imageUrl)
+            }
+        }
+    }
+}
+
 
