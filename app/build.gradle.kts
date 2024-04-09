96ef8f54759fa13dd55cf0781e05b3f62279c33b
fatima204 <fatima.amrin.hossain@gmail.com>
2024-04-09 12:45:15 +0200

    Exercise 4: MovieTrailer is not working (shows the length of the video, but not the video content)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("io.coil-kt:coil-compose:2.6.0")
+
+    implementation("androidx.media3:media3-exoplayer:1.3.0")
+    implementation("androidx.media3:media3-exoplayer-hls:1.3.0")
+    implementation("androidx.media3:media3-ui:1.3.0")
+
 }
--- app/src/main/java/com/example/movieappmad24/widgets/MovieWidgets.kt
+++ app/src/main/java/com/example/movieappmad24/widgets/MovieWidgets.kt
@@ -1,5 +1,6 @@
 package com.example.movieappmad24.widgets
 
+import android.content.Context
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -30,6 +32,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,12 +42,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
@@ -52,7 +58,15 @@ import coil.request.ImageRequest
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.navigation.Screen
 import com.example.movieappmad24.viewmodels.MoviesViewModel
-
+import androidx.media3.common.MediaItem
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.PlayerView
+import com.example.movieappmad24.R
 
 @Composable
 fun MovieList(
@@ -248,3 +262,67 @@ fun HorizontalScrollableImageView(movie: Movie) {
         }
     }
 }
+
+/*@Composable
+fun VideoPlayer(){
+    var lifecycle by remember{
+        mutableStateOf(Lifecycle.Event.ON_CREATE)
+    }
+
+    val context = LocalContext.current
+
+    val mediaItem = MediaItem.fromUri(
+        "android.resource://${context.packageName}/${R.raw.trailer_placeholder}"
+    )
+
+    *//*val resId = context.resources.getIdentifier("trailer_placeholder", "raw", context.packageName)
+    val mediaItem = MediaItem.fromUri("android.resource://${context.packageName}/$resId")
+*//*
+
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            setMediaItem(mediaItem)
+            prepare()
+            playWhenReady = true
+        }
+    }
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(key1 = lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            lifecycle = event
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            exoPlayer.release()
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+
+    }
+
+    AndroidView(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(16f/9f),
+        factory = {
+            PlayerView(context).also {playerView ->
+                playerView.player = exoPlayer
+            }
+        },
+        update = {
+            when(lifecycle){
+                Lifecycle.Event.ON_RESUME -> {
+                    it.onPause()
+                    it.player?.pause()
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    it.onResume()
+                }
+                else -> Unit
+            }
+        }
+    )
+
+}*/
